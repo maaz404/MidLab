26c802025ad71fb0e0c1d33f756f196bb96a5bcd
maaz404 <maazakbar404@gmail.com>
2025-05-01 01:48:54 +0500

    third commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,5 +4,5 @@ import './styles.css';
 function Dashboard() {
 return (
 
-<div className="main"> <h1 className="textColor">Expense Tracker Dashboard</h1> <p className="welcome-text">Welcome! Manage your expenses with ease.</p> </div> ); }
+<div className="main"> <h1 className="textColor">BudgetBuddy Dashboard</h1> <p className="welcome-text">Welcome! Manage your expenses with ease.</p> </div> ); }
 export default Dashboard;
